043068c61aac6222fd0f9aa0f9bc46f0e1668545
Harshita Aggarwal <a.harshitadm@gmail.com>
2024-03-09 16:13:13 -0700

    basic list

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-user_text = input("Enter a task to do:")
-print(user_text)
+prompt = "Enter a task to do:"
+task1 = input(prompt)
+task2 = input(prompt)
+task3 = input(prompt)
+
+tasks = [task1, task2, task3]
+print(tasks)
