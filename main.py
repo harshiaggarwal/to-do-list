acef4491ae016ca6e91ce066a96a131ee22998f8
Harshita Aggarwal <a.harshitadm@gmail.com>
2024-03-10 15:37:57 -0700

    basic while loop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 prompt = "Enter a task to do:"
-task1 = input(prompt)
-task2 = input(prompt)
-task3 = input(prompt)
 
-tasks = [task1, task2, task3]
-print(tasks)
+while True:
+    task = input(prompt)
+    print(task)
+    print("Next...")
