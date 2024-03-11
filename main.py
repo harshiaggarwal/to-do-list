c8fdfd564e39ff8ea6e68dc9c8a647dc98011e99
Harshita Aggarwal <a.harshitadm@gmail.com>
2024-03-11 10:18:16 -0700

    basic for loop

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 tasks = []
 while True:
     action = input("Type add, show or exit:")
+    action = action.strip()
     match action:
         case "add":
             task = input("Enter a task: ")
             tasks.append(task.title())
         case "show":
-            print(tasks)
+            for item in tasks:
+                print(item)
         case "exit":
             print("Bye!")
             break
