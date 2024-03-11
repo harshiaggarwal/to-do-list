26e60d4ddcfd61e8e8a938a08100c49424fac52b
harshiaggarwal <113827333+harshiaggarwal@users.noreply.github.com>
2024-03-11 11:32:29 -0700

    added comments

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 tasks = []
+
+#reads user input and performs action
 while True:
     action = input("Type add, show or exit:")
+    
+    #removing trailing spaces
     action = action.strip()
+    
     match action:
+        #to add a task in the list and change it to title case
         case "add":
             task = input("Enter a task: ")
             tasks.append(task.title())
+        
+        #to display the list    
         case "show":
             for item in tasks:
                 print(item)
+
+        #to exit when done
         case "exit":
             print("Bye!")
             break
