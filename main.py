9a38a329c87340963cc92e2a5555ca9f41abbfaf
Harshita Aggarwal <a.harshitadm@gmail.com>
2024-03-11 15:48:11 -0700

    basic edit and list indexing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ tasks = []
 
 # reads user input and performs action
 while True:
-    action = input("Type add, show or exit:")
+    action = input("Type add, edit, show or exit: ")
 
     # removing trailing spaces
     action = action.strip()
@@ -13,6 +13,14 @@ while True:
             task = input("Enter a task: ")
             tasks.append(task.title())
 
+        # to edit the tasks
+        case "edit":
+            number = input("Enter a task number to edit: ")
+            current_task = tasks[int(number) - 1]
+            print(current_task)
+            new_task = input("Enter the new task: ")
+            tasks[int(number) - 1] = new_task.title()
+
         # to display the list
         case "show" | "display":
             for item in tasks:
@@ -22,5 +30,6 @@ while True:
         case "exit":
             print("Bye!")
             break
+
         case _:
             print("Choose a correct option!")
